ff64daa7fef0f5a704f114f192d700ec9381e466
Kassem804 <knife085420@gmail.com>
2025-09-13 12:51:00 +0200

    Update rc.py

--- python/rc.py
+++ python/rc.py
@@ -1,8 +1,26 @@
 import bs
+import bsGame
 
-my_profile = 'Hi :) My name is Abbas Kassem, I am 16 years old, I am from Algeria'
+my_profile = 'Hi :) My name is Abbas Kassem, I am 16 years old, I am from Algeria\n\n-I like Python Programming, and editing BombSquad Game\n\n-I will be one of the bombsquad editors\n\n-Goodbye ;)'
 
 def text():
+
+    return bs.newNode('text', attrs={
+            'hAttach': 'center',
+            'vAttach': 'center',
+            'hAlign': 'left',
+            'vAlign': 'center',
+            'vrDepth': 0,
+            'scale': 1.0,
+            'shadow': 1.0,
+            'flatness': 1.0,
+            'text': '-',
+            'big': False,
+            'position': (-600, 300),
+            'color': (1,1,0)})
+
+
+def test():
 
     return bs.newNode('text', attrs={
             'hAttach': 'center',
@@ -13,8 +31,8 @@ def text():
             'scale': 1.0,
             'shadow': 1.0,
             'flatness': 1.0,
-            'text': 'BOOYAH',
-            'big': True,
+            'text': '12345678901234567890',
+            'big': False,
             'position': (0, 27),
             'color': (1,1,0)})
 
@@ -27,7 +45,6 @@ def s():
             'texture': bs.getTexture('bar'),
             'position': (0, 27),
             'color': (1,1,0)})
-    i.delete()
 
     n = bs.newNode('text', attrs={
             'hAttach': 'center',
@@ -72,6 +89,17 @@ def add_txt(lifespan, node, txt):
     for i in range(num):
         bs.gameTimer((lifespan/num)*(i+1), a)
 
+def del_txt(lifespan, node, cut):
+    if not node.getNodeType() == 'text': return
+    num = len(node.text)
+    posi = round(num*cut[0])
+    nega = round(-( num*(1.0-cut[1])))
+    assert len(cut) == 2
+    print(str(posi)+'  '+str(nega))
+    def a():
+        node.text = node.text[int(posi) if posi else None:int(nega) if nega else None]
+    a()
+
 def sort_key(item):
     return item[1]
 
@@ -83,9 +111,83 @@ def rk(players):
             'rank': i,
             'player': players,
             'score': score})
-    print(str(enumerate(ranked, start=1)))
     return result
 
 def players_rank():
     players = [('kassem', 150), ('khaled', 120), ('king cold', 220), ('Roshi', 100)]
     return rk(players)
+
+class CutText(bsGame.Actor):
+    def __init__(self, node):
+        bs.Actor.__init__(self)
+        self.node = node
+        self.text_base = self.node.text
+        self.num = len(node.text)
+        self.last_cut = (0.0, 1.0)
+    def del_txt(self, cut):
+        if not self.node.getNodeType() == 'text': return
+        self.posi = round(self.num*cut[0])
+        self.nega = round(-(self.num*(1.0-cut[1])))
+        assert len(cut) == 2
+        self.last_cut = cut
+        print(str(self.posi)+'  '+str(self.nega))
+        self.node.text = self.text_base[int(self.posi) if self.posi else None:int(self.nega) if self.nega else None]
+
+    def anim_cut(self, lifespan, cut):
+        start = abs(self.last_cut[0] - cut[0])
+        end = abs(self.last_cut[1] - cut[1])
+        self.posi1 = round(self.num*start)
+        self.nega1 = round((self.num*(1.0-start)))
+        def del_posi():
+            if cut[0] > self.last_cut[0]:
+                self.del_txt((self.last_cut[0] + (start/self.posi1), self.last_cut[1]))
+            else:
+                self.del_txt((self.last_cut[0] - (start/self.posi1), self.last_cut[1]))
+
+        def del_nega():
+            if cut[1] > self.last_cut[1]:
+                self.del_txt((self.last_cut[0], self.last_cut[1] + (end/self.nega1)))
+            else:
+                self.del_txt((self.last_cut[0], self.last_cut[1] - (end/self.nega1)))
+
+        for i in range(int(self.posi1)):
+            bs.gameTimer((lifespan/int(self.posi1))*(i+1), del_posi)
+
+        for i in range(int(self.nega1)):
+            bs.gameTimer((lifespan/int(self.nega1))*(i+1), del_nega)
+
+class prr(bsGame.Actor):
+  def __init__(self, node):
+    if not node.getNodeType() == 'text': return
+    self.lifespan = 0
+    self.start = 0
+    self.end = 0
+    self.txt = ''
+    self.node = node
+
+
+    self.node.text = str(self.start)
+
+  def add_int(self):
+    if self.start < self.end:
+      self.start += 1
+      if self.txt:
+          self.node.text = self.txt.replace('{num}', str(self.start))
+      else:
+          self.node.text = str(self.start)
+    else:
+      self.start -= 1
+      if self.txt:
+          self.node.text = self.txt.replace('{num}', str(self.start))
+      else:
+          self.node.text = str(self.start)
+
+  def play(self, lifespan, start, end, txt):
+    self.lifespan = lifespan
+    self.start = start
+    self.end = end
+    self.txt = txt
+
+    abc = abs(self.start-self.end)
+    for i in range(abc):
+      bs.gameTimer((self.lifespan/abc)*(i+1), self.add_int)
